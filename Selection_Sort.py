4f46060b0cc66d0ea00383de21925618bb180e13
ga47kaf <marat.serahlazau@tum.de>
2019-10-12 00:35:05 +0200

    [16] Changed manual input to the same style as the rest of the repo.

--- Selection_Sort.py
+++ Selection_Sort.py
@@ -1,4 +1,4 @@
-#Selection Sort
+# Selection Sort
 '''
 Inside the function create a loop with a loop variable i that counts from 0 to the length of the list – 1.
 Create a variable smallest with initial value i.
@@ -10,17 +10,28 @@ Author: Shriya Madan
 '''
 
 
+def selection_sort(alist):
+    """
+    Sorts a given list using a selection sort algorithm
+    :param alist: a list to be sorted
+    :return: None
+    """
 
-def selection_sort(alist):                     #selection sort function 
-    for i in range(0, len(alist) - 1):         #loop for number of elements in alist
-        smallest = i                           #smallest holds the value of i
+    for i in range(0, len(alist) - 1):         # loop for number of elements in alist
+        smallest = i                           # smallest holds the value of i
         for j in range(i + 1, len(alist)):     
-            if alist[j] < alist[smallest]:     #comparing if value at index j is smaller 
-                smallest = j                   #then insert value of j in smallest(so we will get smallest value in this)
-        alist[i], alist[smallest] = alist[smallest], alist[i]     #swapping value of smallest and i
- 
-alist = input('Enter the list of numbers: ').split()     #taking user inputs
-alist = [int(x) for x in alist]                          
-selection_sort(alist)                                    #elements passed to the function for sorting
-print('Sorted list: ', end='')    
-print(alist)                                             #sorted array is printed
+            if alist[j] < alist[smallest]:     # comparing if value at index j is smaller
+                smallest = j                   # then insert value of j in smallest(so we will get smallest value in this)
+        alist[i], alist[smallest] = alist[smallest], alist[i]     # swapping value of smallest and i
+
+
+if __name__ == '__main__':
+
+    arr = [1, 12, 11, -2, 13, 0, 6, 7]
+    print ('Given array is', end='\n')
+    print(*arr)
+
+    selection_sort(arr)
+
+    print('Sorted array is:', end='\n')
+    print(*arr)
